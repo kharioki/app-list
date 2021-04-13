78b49e862197cef9a0c6dd6fb9eac716ae8cf38c
kharioki <antolisah@gmail.com>
2021-04-13 15:06:05 +0300

    pages and routes

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return ( 
+        <div>
+          <h1>About</h1>
+          <p>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
+          <p>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
+          <p>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
+        </div>
+    );
+}
+ 
+export default About;
---
+++ pages/apps/index.js
@@ -0,0 +1,9 @@
+const Apps = () => {
+    return (
+        <div>
+            <h1>My Apps</h1>
+        </div>
+    );
+}
+ 
+export default Apps;
