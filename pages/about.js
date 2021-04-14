0f766e5c84ed4c66e409459b9f8e41464390c153
kharioki <antolisah@gmail.com>
2021-04-15 01:00:06 +0300

    redirecting with useRouter

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,9 @@ const About = () => {
     return ( 
         <div>
           <h1>About</h1>
-          <p>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
-          <p>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
-          <p>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
-import Head from 'next/head';
 import Link from 'next/link';
-
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
       <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
-      <p className={styles.text}>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
-      <p className={styles.text}>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
 
       <Link href="/apps">
         <a className={styles.btn}>See my apps list</a>
