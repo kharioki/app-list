f126f53c232ab465c9988f1d669fd22a31bf57b1
kharioki <antolisah@gmail.com>
2021-04-15 00:46:36 +0300

    style modules and 404 page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import Link from 'next/link';
 
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
-      <p>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
-      <p>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
+      <p className={styles.text}>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
+      <p className={styles.text}>lorem, ipsum dolor sit amet consectetur adipisicing elit. Nuquam facilis cum, voluptatibus</p>
 
       <Link href="/apps">
-        <a>See my apps list</a>
+        <a className={styles.btn}>See my apps list</a>
       </Link>
     </div>
   )
